2b04cacc61ad2f267862b237ee73feca947c95b4
Thomas S <thomas.scy@gmail.com>
2023-02-06 13:27:00 +0100

    add fav pizzas

---
+++ favouritePizzas.js
@@ -0,0 +1,9 @@
+[
+    {
+      id: 1,
+      name: "Marinara",
+      ingredients: [ "San Marzano tomato sauce", "garlic", "oregano", "olive oil" ]
+      price: 3200
+      allergens: [1,3,9]
+    }
+]

3188d6375cc4a2ae749ba0cb9e17898faf5838e7
Thomas S <thomas.scy@gmail.com>
2023-02-06 13:35:41 +0100

    changed js to json

--- favouritePizzas.js
+++
@@ -1,9 +0,0 @@
-[
-    {
-      id: 1,
-      name: "Marinara",
-      ingredients: [ "San Marzano tomato sauce", "garlic", "oregano", "olive oil" ]
-      price: 3200
-      allergens: [1,3,9]
-    }
-]

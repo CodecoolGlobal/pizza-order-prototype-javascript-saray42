cf11428e76af4e7163b079365b1f72dbb3ff2f3a
San <san.kuno22@gmail.com>
2023-02-08 14:14:12 +0100

    nothing

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,9 @@ const path = require("path");
 const path = require("path");
 
 const pizzaList = JSON.parse(fs.readFileSync(__dirname + "/pizza-list.json"));
-const allergeneList = JSON.parse(fs.readFileSync(__dirname + "/allergens-list.json"));
+const allergeneList = JSON.parse(
+  fs.readFileSync(__dirname + "/allergens-list.json")
+);
 
 const orders = __dirname + "/orders.json";
 
@@ -17,44 +19,37 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-<<<<<<< HEAD
-    console.log(__dirname)
-    // res.sendFile(path.join(`${__dirname}/../frontend/index.html`));
-    res.send(__dirname + "/frontend/index.html");
-
-
-
-=======
-    res.sendFile(path.join(__dirname + "/../frontend/index.html"));
->>>>>>> 9568648868d54e341d86da56410f5b709f528e07
+  res.sendFile(path.join(__dirname + "/../frontend/index.html"));
 });
 
-app.get/"/menu", (req, res) => {
+app.get / "/menu",
+  (req, res) => {
     res.sendFile(path.join(__dirname + "/../frontend/menu.html"));
-}
+  };
 
 app.get("/api/pizza", (req, res) => {
-    res.send(pizzaList);
+  res.send(pizzaList);
 });
 
 app.get("/api/allergens", (req, res) => {
-    res.send(allergeneList);
+  res.send(allergeneList);
 });
 
-app.route("/api/order")
-    .get(async (req, res) => {
-        const orderList = await fileReaderAsync(orders);
-        res.status(201).send(JSON.parse(orderList));
-    })
-    .post(async (req, res) => {
-        const incomingOrder = req.body;
-        let orderList = await fileReaderAsync(orders);
-        orderList = JSON.parse(orderList);
-        orderList.push(incomingOrder);
-        await fileWriterAsync(orders, JSON.stringify(orderList));
-        res.send(orderList);
-    });
+app
+  .route("/api/order")
+  .get(async (req, res) => {
+    const orderList = await fileReaderAsync(orders);
+    res.status(201).send(JSON.parse(orderList));
+  })
+  .post(async (req, res) => {
+    const incomingOrder = req.body;
+    let orderList = await fileReaderAsync(orders);
+    orderList = JSON.parse(orderList);
+    orderList.push(incomingOrder);
+    await fileWriterAsync(orders, JSON.stringify(orderList));
+    res.send(orderList);
+  });
 
 app.use(express.static(path.join(__dirname + "/../frontend/public")));
 
-app.listen(port, _ => console.log(`http://127.0.0.1:${port}`));
+app.listen(port, (_) => console.log(`http://127.0.0.1:${port}`));
